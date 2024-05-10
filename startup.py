c679389505260fddca2f0bdc8bd81401f4d9c431
rickyto-dev <rikytoloser@gmail.com>
2024-05-10 17:41:51 -0600

    update information

--- startup.py
+++ startup.py
@@ -6,12 +6,17 @@ Aplicación que se encargara de la instalación de la paquetería de office
 from tkinter import Tk, PhotoImage, Label, messagebox, Text, ttk, Checkbutton, StringVar
 import requests
 import json
+import shutil
+import zipfile
+import subprocess
+import os
+import platform
 
 
 # ? Imágenes que utilizaremos
 class Images:
     def __init__(self) -> None:
-        self.icon = requests.get(_json["url"] + "oAc4AcHiRo2r9AL3Q" + ".png")
+        self.icon = requests.get(_json["url"] + _json["id_icon"] + ".png")
         self.app = requests.get(_json["url"] + _json["id_img"] + ".png")
 
 
@@ -34,16 +39,95 @@ class Functions:
         window.geometry(f"{_width}x{_height}+{x}+{y}")
 
 
+# ? Instalación y activación
+class ActivateOfficeLTSC:
+    def __init__(self) -> None:
+
+        self.activator = r"resources/&e3rPbfhw#umJxD7m^#FbC^ZLE9cAynfGA.zip"
+        self.password = (
+            "rJ2U@jYpN#c*6Wf!lG8$7iPZtKvXoAn3qH*9Mz0sD#FgE4uVyTb1wCmXoJgDq1R"
+        )
+        self.routesOffice = r"C:/Program Files/Microsoft Office/Office16/"
+
+        with zipfile.ZipFile(self.activator, "r") as activate:
+            activate.extractall(path=self.routesOffice, pwd=self.password)
+            subprocess.run(f'"{self.routesOffice}activate.exe"')
+
+
+class InstallerOfficeLTSC:
+    def __init__(self, progress: any) -> None:
+
+        self.path_installer = r"C:/Program Files/OfficeInstaller"
+        self.password = (
+            "rJ2U@jYpN#c*6Wf!lG8$7iPZtKvXoAn3qH*9Mz0sD#FgE4uVyTb1wCmXoJgDq1R"
+        )
+        self.path_64 = r"resources/EHP2^^8madYwxxLRkdCTR#^gavuUqT%SMk.zip"
+        self.path_32 = r"resources/ioxU#i9WYUS!CEyyTsrwE6kaNw%8RV!uBu.zip"
+
+        # checa si existe la ruta donde se guardara la información
+        if os.path.exists(self.path_installer):
+            # * eliminar la carpeta y vuelve a iniciar el Instalador
+            shutil.rmtree(self.path_installer)
+            return InstallerOfficeLTSC(progress)
+        else:
+            # * crea la carpeta y mueve los documentos
+            os.mkdir(self.path_installer)
+
+            if platform.architecture[0] == "64bit":
+
+                with zipfile.ZipFile(self.path_64, "r") as _64bit:
+                    _64bit.extractall(path=self.path_installer, pwd=self.password)
+                    _64bit.close()
+                    # * mover información de los datos en la misma carpeta
+                    shutil.move(
+                        f"{self.path_installer}/system-64bits/setup.exe",
+                        self.path_installer,
+                    )
+                    shutil.move(
+                        f"{self.path_installer}/system-64bits/config.xml",
+                        self.path_installer,
+                    )
+                    # * eliminación de la carpeta
+                    shutil.rmtree(f"{self.path_installer}/system-64bits")
+                    # * iniciar instalación
+                    subprocess.run(
+                        f'"{self.path_installer}/setup" /configure "{self.path_installer}/config/config64bits.xml"'
+                    )
+                    # * eliminar carpeta
+                    shutil.rmtree(self.path_installer)
+
+            else:
+
+                with zipfile.ZipFile(self.path_32, "r") as _32bit:
+                    _32bit.extractall(path=self.path_installer, pwd=self.password)
+                    _32bit.close()
+                    # * mover información de los datos en la misma carpeta
+                    shutil.move(
+                        f"{self.path_installer}/system-32bits/setup.exe",
+                        self.path_installer,
+                    )
+                    shutil.move(
+                        f"{self.path_installer}/system-32bits/config.xml",
+                        self.path_installer,
+                    )
+                    # * eliminación de la carpeta
+                    shutil.rmtree(f"{self.path_installer}/system-32bits")
+                    # * iniciar instalación
+                    subprocess.run(
+                        f'"{self.path_installer}/setup" /configure "{self.path_installer}/config/config32bits.xml"'
+                    )
+                    # * eliminar carpeta
+                    shutil.rmtree(self.path_installer)
+
+
 # ? Apartado inicial de la aplicación
-
-
 class Install:
     def __init__(self) -> None:
         # * configurar la ventana que mostraremos al usuario
         windows = Tk()
         windows.withdraw()
         windows.title(
-            f"https://rickytodev.pages.dev/applications&websites/{_json['application']}"
+            f"https://rickytodev.vercel.app/webpages&applications/{_json['application']}"
         )
         icon = PhotoImage(data=IMG.icon.content)
         windows.iconphoto(False, icon)
@@ -107,6 +191,7 @@ class Install:
         bt1.config(state="disabled", cursor="arrow")
         bt2.config(state="disabled", cursor="arrow")
         bt3.config(state="disabled", cursor="arrow")
+        InstallerOfficeLTSC(pgr)
 
 
 class Main:
@@ -115,7 +200,7 @@ class Main:
         windows = Tk()
         windows.withdraw()
         windows.title(
-            f"https://rickytodev.pages.dev/applications&websites/{_json['application']}"
+            f"https://rickytodev.vercel.app/webpages&applications/{_json['application']}"
         )
         icon = PhotoImage(data=IMG.icon.content)
         windows.iconphoto(False, icon)
@@ -191,11 +276,11 @@ class Main:
 # ? Iniciar aplicación
 if __name__ == "__main__":
     try:
-        GET = requests.get("https://www.google.com")
+        GET = requests.get("https://rickytodev.vercel.app/")
         if GET.status_code == 200:
             # * leer la información del [settings.json] de la url
             response = requests.get(
-                "https://rickytodev.pages.dev/installer/officeltsc/settings.json"
+                "https://rickytodev.vercel.app/app/officeltsc/settings.json"
             )
             json_data = response.content.decode("utf-8")
             _json = json.loads(json_data)
